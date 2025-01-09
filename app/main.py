a83010f026297cac48549b2cdc39a26ff220e72b
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-09 17:50:02 +0300

    Refactor command handling in main loop to introduce a dedicated function for command existence checks, improving path resolution for commands and enhancing feedback for unrecognized commands.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 import os
 import sys
 
+PATH = os.environ.get("PATH")
+PATH_LIST = PATH.split(os.pathsep)
+
+
+def check_command_exists(command: str) -> tuple[bool, str | None]:
+    for path in PATH_LIST:
+        cmd_path = os.path.join(path, command)
+        if os.path.exists(cmd_path):
+            return True, cmd_path
+    return False, None
+
 
 def main():
-
-    PATH = os.environ.get("PATH")
-    PATH_LIST = PATH.split(os.pathsep)
 
     while True:
         sys.stdout.write("$ ")
@@ -23,23 +31,25 @@ def main():
         cmd = command_parts[0]
         arguments = command_parts[1:]
 
-        if cmd not in built_in_commands:
-            sys.stdout.write(f"{cmd}: command not found\n")
-        elif cmd == "exit":
+        if cmd == "exit":
             sys.exit(int(arguments[0]))
         elif cmd == "echo":
             sys.stdout.write(" ".join(arguments) + "\n")
         elif cmd == "type":
-            if arguments in built_in_commands:
-                sys.stdout.write(f"{arguments} is a shell builtin\n")
+            if arguments[0] in built_in_commands:
+                sys.stdout.write(f"{arguments[0]} is a shell builtin\n")
             else:
-                for path in PATH_LIST:
-                    cmd_path = os.path.join(path, arguments)
-                    if os.path.exists(cmd_path):
-                        sys.stdout.write(f"{arguments} is {cmd_path}\n")
-                        break
+                cmd_exists, cmd_path = check_command_exists(arguments[0])
+                if cmd_exists:
+                    sys.stdout.write(f"{arguments[0]} is {cmd_path}\n")
                 else:
-                    sys.stdout.write(f"{arguments}: not found\n")
+                    sys.stdout.write(f"{arguments[0]}: not found\n")
+        elif cmd not in built_in_commands:
+            cmd_exists, cmd_path = check_command_exists(cmd)
+            if cmd_exists:
+                os.system(command)  # nosec: B605
+            else:
+                sys.stdout.write(f"{cmd}: command not found\n")
 
 
 if __name__ == "__main__":
