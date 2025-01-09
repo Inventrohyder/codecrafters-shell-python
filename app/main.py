da845833d28a2421cac7c5c065de02108da31bdb
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-09 17:03:22 +0300

    Refactor command handling in main loop to include 'type' command and improve feedback for unrecognized commands

--- app/main.py
+++ app/main.py
@@ -7,13 +7,25 @@ def main():
 
         command = input()
 
+        built_in_commands = {
+            "echo",
+            "exit",
+            "type",
+        }
+
         command_parts = command.split(sep=" ")
-        if command_parts[0] == "exit":
+
+        if command_parts[0] not in built_in_commands:
+            sys.stdout.write(f"{command}: command not found\n")
+        elif command_parts[0] == "exit":
             sys.exit(int(command_parts[1]))
         elif command_parts[0] == "echo":
             sys.stdout.write(" ".join(command_parts[1:]) + "\n")
-        else:
-            sys.stdout.write(f"{command}: command not found\n")
+        elif command_parts[0] == "type":
+            if command_parts[1] in built_in_commands:
+                sys.stdout.write(f"{command_parts[1]} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{command_parts[1]}: not found\n")
 
 
 if __name__ == "__main__":
