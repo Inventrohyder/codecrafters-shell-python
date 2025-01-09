8e35c22d41300bb34262d504a6bc64b416e4f182
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-09 12:45:18 +0300

    Refactor main loop to continuously prompt for user input and provide feedback for unrecognized commands

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
+        # Wait for user input
+        command = input()
 
-    sys.stdout.write(f"{command}: command not found\n")
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
