e202c6718c33e41d16190bda76529197c3033350
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-09 17:37:25 +0300

    Refactor command handling in main loop to streamline command parsing and improve feedback for unrecognized commands

--- app/main.py
+++ app/main.py
@@ -20,23 +20,26 @@ def main():
 
         command_parts = command.split(sep=" ")
 
-        if command_parts[0] not in built_in_commands:
-            sys.stdout.write(f"{command_parts[0]}: command not found\n")
-        elif command_parts[0] == "exit":
-            sys.exit(int(command_parts[1]))
-        elif command_parts[0] == "echo":
-            sys.stdout.write(" ".join(command_parts[1:]) + "\n")
-        elif command_parts[0] == "type":
-            if command_parts[1] in built_in_commands:
-                sys.stdout.write(f"{command_parts[1]} is a shell builtin\n")
+        cmd = command_parts[0]
+        arguments = command_parts[1:]
+
+        if cmd not in built_in_commands:
+            sys.stdout.write(f"{cmd}: command not found\n")
+        elif cmd == "exit":
+            sys.exit(int(arguments[0]))
+        elif cmd == "echo":
+            sys.stdout.write(" ".join(arguments) + "\n")
+        elif cmd == "type":
+            if arguments in built_in_commands:
+                sys.stdout.write(f"{arguments} is a shell builtin\n")
             else:
                 for path in PATH_LIST:
-                    cmd_path = os.path.join(path, command_parts[1])
+                    cmd_path = os.path.join(path, arguments)
                     if os.path.exists(cmd_path):
-                        sys.stdout.write(f"{command_parts[1]} is {cmd_path}\n")
+                        sys.stdout.write(f"{arguments} is {cmd_path}\n")
                         break
                 else:
-                    sys.stdout.write(f"{command_parts[1]}: not found\n")
+                    sys.stdout.write(f"{arguments}: not found\n")
 
 
 if __name__ == "__main__":
