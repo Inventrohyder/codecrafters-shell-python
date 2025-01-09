be4b69a8f70ec69f36ae3e127091e90977325567
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-09 12:54:10 +0300

    Add exit command handling to main input loop

--- app/main.py
+++ app/main.py
@@ -8,6 +8,11 @@ def main():
         # Wait for user input
         command = input()
 
+        # if command is exit, call the exit builtin
+        command_parts = command.split()
+        if command_parts[0] == "exit":
+            sys.exit(command_parts[1])
+
         sys.stdout.write(f"{command}: command not found\n")
 
 
