ca04fc3b24ee3d30cfd188a7e79b6c9efb45158e
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-09 16:54:50 +0300

    Enhance command handling in main loop to support 'echo' command and improve feedback for unrecognized commands

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ def main():
     while True:
         sys.stdout.write("$ ")
 
-        # Wait for user input
         command = input()
 
-        # if command is exit, call the exit builtin
-        command_parts = command.split()
+        command_parts = command.split(sep=" ")
         if command_parts[0] == "exit":
             sys.exit(int(command_parts[1]))
-
-        sys.stdout.write(f"{command}: command not found\n")
+        elif command_parts[0] == "echo":
+            sys.stdout.write(" ".join(command_parts[1:]) + "\n")
+        else:
+            sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
