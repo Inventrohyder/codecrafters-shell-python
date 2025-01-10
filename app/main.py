29d9e6091a6bdde72c117bc9eb049a4baee8c8ef
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-01-10 21:36:01 +0300

    Add 'cd' command to main command handling and refactor command existence check function

--- app/main.py
+++ app/main.py
@@ -1,16 +1,62 @@
 import os
 import sys
+from typing import Callable
 
 PATH = os.environ.get("PATH")
 PATH_LIST = PATH.split(os.pathsep)
 
+BUILT_IN_COMMANDS: set[str] = {
+    "cd",
+    "echo",
+    "exit",
+    "pwd",
+    "type",
+}
 
-def check_command_exists(command: str) -> tuple[bool, str | None]:
+
+def _check_command_exists(command: str) -> tuple[bool, str | None]:
     for path in PATH_LIST:
         cmd_path = os.path.join(path, command)
         if os.path.exists(cmd_path):
             return True, cmd_path
     return False, None
+
+
+def cd(arguments: list[str]) -> None:
+    if len(arguments) == 0:
+        os.chdir(os.getenv("HOME"))
+        return
+
+    try:
+        os.chdir(arguments[0])
+    except FileNotFoundError:
+        sys.stdout.write(f"cd: {arguments[0]}: No such file or directory\n")
+        return
+
+
+def echo(arguments: list[str]) -> None:
+    sys.stdout.write(" ".join(arguments) + "\n")
+
+
+def exit(arguments: list[str]) -> None:
+    sys.exit(int(arguments[0]))
+
+
+def pwd(_: list[str]) -> None:
+    sys.stdout.write(f"{os.getcwd()}\n")
+
+
+def type(arguments: list[str]) -> None:
+    if arguments[0] in BUILT_IN_COMMANDS:
+        sys.stdout.write(f"{arguments[0]} is a shell builtin\n")
+        return
+
+    cmd_exists, cmd_path = _check_command_exists(arguments[0])
+    if cmd_exists:
+        sys.stdout.write(f"{arguments[0]} is {cmd_path}\n")
+        return
+
+    sys.stdout.write(f"{arguments[0]}: not found\n")
 
 
 def main():
@@ -20,39 +66,32 @@ def main():
 
         command = input()
 
-        built_in_commands = {
-            "echo",
-            "exit",
-            "pwd",
-            "type",
+        command_parts: list[str] = command.split(sep=" ")
+
+        cmd: str = command_parts[0]
+        arguments: list[str] = command_parts[1:]
+
+        built_in_commands_to_programs_map: dict[str, Callable[[list[str]], None]] = {
+            "echo": echo,
+            "exit": exit,
+            "pwd": pwd,
+            "type": type,
+            "cd": cd,
         }
 
-        command_parts = command.split(sep=" ")
+        program_func: Callable[[list[str]], None] | None = (
+            built_in_commands_to_programs_map.get(cmd, None)
+        )
 
-        cmd = command_parts[0]
-        arguments = command_parts[1:]
-
-        if cmd == "exit":
-            sys.exit(int(arguments[0]))
-        elif cmd == "echo":
-            sys.stdout.write(" ".join(arguments) + "\n")
-        elif cmd == "type":
-            if arguments[0] in built_in_commands:
-                sys.stdout.write(f"{arguments[0]} is a shell builtin\n")
-            else:
-                cmd_exists, cmd_path = check_command_exists(arguments[0])
-                if cmd_exists:
-                    sys.stdout.write(f"{arguments[0]} is {cmd_path}\n")
-                else:
-                    sys.stdout.write(f"{arguments[0]}: not found\n")
-        elif cmd == "pwd":
-            sys.stdout.write(os.getcwd() + "\n")
-        elif cmd not in built_in_commands:
-            cmd_exists, cmd_path = check_command_exists(cmd)
+        if program_func is None:
+            cmd_exists, _ = _check_command_exists(cmd)
             if cmd_exists:
                 os.system(command)  # nosec: B605
             else:
                 sys.stdout.write(f"{cmd}: command not found\n")
+            continue
+
+        program_func(arguments)
 
 
 if __name__ == "__main__":
